0cd68fc38f2c253e621aa50776f4c9d882899e08
yukitsushima <tsushima.y.aa@m.titech.ac.jp>
2020-05-14 00:10:56 +0900

    atomic

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -12,11 +12,14 @@ int main() {
   }
   printf("\n");
 
-  std::vector<int> bucket(range); 
+  std::vector<int> bucket(range);
+  #pragma omp parallel for
   for (int i=0; i<range; i++) {
     bucket[i] = 0;
   }
+  #pragma omp parallel for shared(bucket)
   for (int i=0; i<n; i++) {
+    #pragma omp atomic update
     bucket[key[i]]++;
   }
   for (int i=0, j=0; i<range; i++) {

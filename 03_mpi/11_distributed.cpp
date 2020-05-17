cd5f5121a755e679dcdd325f4ceaa3ea34af58d1
yukitsushima <tsushima.y.aa@m.titech.ac.jp>
2020-05-17 21:01:54 +0900

    競合阻止

--- 03_mpi/11_distributed.cpp
+++ 03_mpi/11_distributed.cpp
@@ -27,11 +27,17 @@ int main(int argc, char** argv) {
   MPI_Type_contiguous(5, MPI_DOUBLE, &MPI_BODY);
   MPI_Type_commit(&MPI_BODY);
   for(int irank=0; irank<size; irank++) {
+    Body recv_jbody[N/size];
     MPI_Win win;
-    MPI_Win_create(jbody,N/size*sizeof(Body),sizeof(Body),MPI_INFO_NULL,MPI_COMM_WORLD,&win);
+    //To avoid the collision of read with write, allocate different memory 
+    MPI_Win_create(recv_jbody,N/size*sizeof(Body),sizeof(Body),MPI_INFO_NULL,MPI_COMM_WORLD,&win);
     MPI_Win_fence(0,win);
     MPI_Put(jbody,N/size,MPI_BODY,send_to,0,N/size,MPI_BODY,win);
     MPI_Win_fence(0,win);
+    //write back
+    for(int i=0;i<N/size;i++){
+      jbody[i]=recv_jbody[i];
+    }
 
     //MPI_Send(jbody, N/size, MPI_BODY, send_to, 0, MPI_COMM_WORLD);
     //MPI_Recv(jbody, N/size, MPI_BODY, recv_from, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
